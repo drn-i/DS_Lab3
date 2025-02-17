70d4709311137d31908d2703e44f1a0041cf3e4e
Hassan Ali <hsns.a999@gmail.com>
2025-02-17 09:31:24 +0000

    Files Uploaded

---
+++ Lab3_WordCount/src/main/java/edu/hassan/RunnerClass.java
@@ -0,0 +1,27 @@
+package edu.hassan;
+import java.io.IOException;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
+public class RunnerClass {
+    public static void main(String[] args) throws IOException{
+        JobConf conf = new JobConf(RunnerClass.class);
+        conf.setJobName("WordCount");
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+        conf.setMapperClass(MapperClass.class);
+        conf.setCombinerClass(ReducerClass.class);
+        conf.setReducerClass(ReducerClass.class);
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+        FileInputFormat.setInputPaths(conf,new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf,new Path(args[1]));
+        JobClient.runJob(conf);
+    }
+}
